69d5ceb89e0432cc460756bbdb3b1393b94507ae
vincentpc <peter5547825@gmail.com>
2013-10-18 15:23:04 +0800

    Update wechat.py

--- handlers/wechat.py
+++ handlers/wechat.py
@@ -35,7 +35,7 @@ class WeChat(object):
                         self.ToUserName,
                         time.time(),
                         url,
-                        hqurl,
+                        url,
                         funcflag
             )
 
