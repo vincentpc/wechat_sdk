1e6bb21232fe242002a1b59e6ce40527d83f9234
vincentpc <peter5547825@gmail.com>
2013-10-19 14:36:29 +0800

    Update yixinmenu.py

---
+++ menu/yixinmenu.py
@@ -0,0 +1,50 @@
+# encoding: utf-8
+
+# author hzlzh
+# must be python 2.x
+
+import json
+import urllib
+import urllib2
+
+appid = "c05988627383467d87272456e5ae8b45"
+secret = "36f21ca8841e49488240b0a82c5f7ee8"
+access_url = "https://api.yixin.im/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
+menu_url = "https://api.yixin.im/cgi-bin/menu/create?%s"
+
+
+def get_access_token():
+    f = urllib.urlopen(access_url % (appid, secret))
+    resp = json.loads(f.read())
+    return resp['access_token']
+
+
+def generate_menu(token):
+    menus = {
+        "button": [
+            {
+                "type": "click",
+                "name": "帮助",
+                "key": "help"
+            },
+            {
+                "type": "view",
+                "name": "关于",
+                "url": "about"
+            }]
+    }
+    params = {'access_token': urllib.quote(token)}
+    url = menu_url % urllib.urlencode(params)
+    print url
+    request = urllib2.Request(url, json.dumps(menus, ensure_ascii=False))
+    response = urllib2.urlopen(request)
+    print response.read()
+
+
+def main():
+    token = get_access_token()
+    generate_menu(token)
+
+
+if __name__ == '__main__':
+    main()
