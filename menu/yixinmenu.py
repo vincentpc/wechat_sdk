817c356ae8faae4fb23c53591a86cdaf9cf27ec8
vincentpc <peter5547825@gmail.com>
2013-10-19 14:42:05 +0800

    Update yixinmenu.py

--- menu/yixinmenu.py
+++ menu/yixinmenu.py
@@ -11,7 +11,7 @@ appid = "c05988627383467d87272456e5ae8b45"
 secret = "36f21ca8841e49488240b0a82c5f7ee8"
 access_url = "https://api.yixin.im/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
 menu_url = "https://api.yixin.im/cgi-bin/menu/create?%s"
-
+get_menu_url = "https://api.yixin.im/cgi-bin/menu/get?%s"
 
 def get_access_token():
     f = urllib.urlopen(access_url % (appid, secret))
@@ -41,10 +41,19 @@ def generate_menu(token):
     print response.read()
 
 
+
+def get_menu(token):
+    params = {'access_token': urllib.quote(token)}
+    url = get_menu_url % urllib.urlencode(params)
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    resp = json.loads(f.read())
+    print resp
+    
 def main():
     token = get_access_token()
-    generate_menu(token)
-
+    #generate_menu(token)
+    get_menu(token)
 
 if __name__ == '__main__':
     main()
