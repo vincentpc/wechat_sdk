99969ac45af7b9aaae4b955465557911c9c3adb5
vincentpc <peter5547825@gmail.com>
2013-10-19 14:46:35 +0800

    Update wechatmenu.py

--- menu/wechatmenu.py
+++ menu/wechatmenu.py
@@ -11,12 +11,11 @@ appid = ""
 secret = ""
 access_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
 menu_url = "https://api.weixin.qq.com/cgi-bin/menu/create?%s"
-
+get_menu_url = "https://api.weixin.qq.com/cgi-bin/menu/get?%s"
 
 def get_access_token():
     f = urllib.urlopen(access_url % (appid, secret))
     resp = json.loads(f.read())
-    print resp
     return resp['access_token']
 
 
@@ -25,26 +24,36 @@ def generate_menu(token):
         "button": [
             {
                 "type": "click",
-                "name": "这是什么？",
-                "key": "helpMenu"
+                "name": "帮助",
+                "key": "help"
             },
             {
                 "type": "view",
-                "name": "(miao.in)",
-                "url": "http://miao.in/"
+                "name": "关于",
+                "url": "about"
             }]
     }
     params = {'access_token': urllib.quote(token)}
     url = menu_url % urllib.urlencode(params)
+    print url
     request = urllib2.Request(url, json.dumps(menus, ensure_ascii=False))
     response = urllib2.urlopen(request)
     print response.read()
 
 
+
+def get_menu(token):
+    params = {'access_token': urllib.quote(token)}
+    url = get_menu_url % urllib.urlencode(params)
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    resp = json.loads(response.read())
+    print resp
+    
 def main():
     token = get_access_token()
     generate_menu(token)
-
+    #get_menu(token)
 
 if __name__ == '__main__':
     main()
